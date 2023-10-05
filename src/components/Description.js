17ae952a008db30bfb56e44e27874a14d6d3e590
599021HVL <wlloyd02@hotmail.com>
2023-10-05 18:32:22 +0200

    ny

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,23 +1,39 @@
 import React from "react";
-import { Typography, Box } from "@mui/material";
+import { Avatar, Typography, Box } from "@mui/material";
 
 function Description() {
   return (
-    <><Box py={5} bgcolor="#e8eaf6" borderRadius={3} boxShadow={3} mt={4} px={3}>
-      <Typography variant="h6" gutterBottom color="#3f51b5">
-        Om Meg
-      </Typography>
-      <Typography>
-        hei eg kommer fra øygarden, eg har grod denne blomsten, fin?
-      </Typography>
-    </Box><Box py={5} bgcolor="#e8eaf6" borderRadius={3} boxShadow={3} mt={4} px={3}>
+    <Box>
+      <Box py={5} bgcolor="#e8eaf6" borderRadius={3} boxShadow={3} mt={4} px={3}>
+        <Typography variant="h6" gutterBottom color="#3f51b5">
+          Om Meg
+        </Typography>
+        <Typography>
+          hei eg kommer fra øygarden, eg har grodd denne blomsten, fin?
+        </Typography>
+      </Box>
+
+      <Avatar
+        alt="Blomst"
+        sx={{
+          width: 150,
+          height: 150,
+          margin: "auto",
+          border: "4px solid #3f51b5",
+        }}
+      >
+        <img src="/bilde-av-kompis.jpeg" alt="Blomst" />
+      </Avatar>
+
+      <Box py={5} bgcolor="#e8eaf6" borderRadius={3} boxShadow={3} mt={4} px={3}>
         <Typography variant="h6" gutterBottom color="#3f51b5">
           Om andre
         </Typography>
         <Typography>
           hei han kommer fra sveits, han er skadet
         </Typography>
-      </Box></>
+      </Box>
+    </Box>
   );
 }
 
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -22,16 +22,7 @@ function Profile() {
           border: "4px solid #3f51b5",
         }}
       />
-      <Avatar
-        src="/bilde-av-kompis.jpeg"
-        alt="Blomst"
-        sx={{
-          width: 150,
-          height: 150,
-          margin: "auto",
-          border: "4px solid #3f51b5",
-        }}
-      />
+
       <Typography variant="h4" gutterBottom color="#3f51b5">
         sup
       </Typography>
