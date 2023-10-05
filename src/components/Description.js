12371f8987a59aeb0cf1615f776744e0a305d5e9
599021HVL <wlloyd02@hotmail.com>
2023-10-05 18:03:04 +0200

    ny box

--- src/components/Description.js
+++ src/components/Description.js
@@ -3,14 +3,21 @@ import { Typography, Box } from "@mui/material";
 
 function Description() {
   return (
-    <Box py={5} bgcolor="#e8eaf6" borderRadius={3} boxShadow={3} mt={4} px={3}>
+    <><Box py={5} bgcolor="#e8eaf6" borderRadius={3} boxShadow={3} mt={4} px={3}>
       <Typography variant="h6" gutterBottom color="#3f51b5">
         Om Meg
       </Typography>
       <Typography>
-      hei eg kommer fra øygarden, eg har grod denne blomsten, fin?
+        hei eg kommer fra øygarden, eg har grod denne blomsten, fin?
       </Typography>
-    </Box>
+    </Box><Box py={5} bgcolor="#e8eaf6" borderRadius={3} boxShadow={3} mt={4} px={3}>
+        <Typography variant="h6" gutterBottom color="#3f51b5">
+          Om andre
+        </Typography>
+        <Typography>
+          hei han kommer fra sveits, han er skadet
+        </Typography>
+      </Box></>
   );
 }
 
